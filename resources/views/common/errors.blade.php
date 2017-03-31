1b3dcc7b7b79d0198419b7496e735670f2033f2f
Alex Kiura <kiuraalex@gmail.com>
2017-03-31 14:32:32 +0300

    chore: add errors view

---
+++ resources/views/common/errors.blade.php
@@ -0,0 +1,14 @@
+@if(count($errors) > 0) {
+    <!-- Form error list -->
+    <div class="alert alert-danger">
+        <strong>
+            Whoops! Something went wrong!
+        </strong>
+        <hr>
+        <ul>
+            @foreach($errors->all() as error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+}
