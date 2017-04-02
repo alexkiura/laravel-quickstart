fe0dd4203fc8050df932ada22d2c1f2de90837ee
Alex Kiura <kiuraalex@gmail.com>
2017-04-02 10:31:26 +0300

    feat: add view to list all tasks

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -3,8 +3,10 @@
 @section('content')
 
     <div class="panel-body">
+        <!-- Validation errors -->
         @include('common.errors')
 
+        <!--New Task form -->
         <form action="/task" method="POST" class="form-horizontal">
             {{ crsf_field() }}
             
@@ -25,6 +27,38 @@
                 </div>
             </div>
         </form>
+
+        <!-- Current tassks -->
+        @if (count($tasks) > 0)
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Current Tasks
+                </div>
+
+                <div class="panel-body">
+                    <table class="table table-striped task-table">
+                        <!-- Table headings -->
+                        <thead>
+                            <th>Task</th>
+                            <th>&nbsp;</th>
+                        </thead>
+                        <!-- Table Body -->
+                        <tbody>
+                            @foreach ($tasks as $task)
+                                <tr>
+                                    <!-- Task name -->
+                                    <td class="table-text">
+                                        <div>{{ $task->name}}</div>
+                                    </td>
+                                    <td>
+                                        <!-- Delete button -->
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
     </div>
 
 @endsection
